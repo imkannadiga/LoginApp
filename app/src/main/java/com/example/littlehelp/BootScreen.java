5543a35d4e2b915eebaca0d62c48ba309875549e
Vaishnavi02309 <90596327+Vaishnavi02309@users.noreply.github.com>
2021-09-22 23:34:23 +0530

    New UI

---
+++ app/src/main/java/com/example/littlehelp/BootScreen.java
@@ -0,0 +1,14 @@
+package com.example.littlehelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BootScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boot_screen);
+    }
+}
